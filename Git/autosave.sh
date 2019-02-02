94d044206717ffb53eb38e939be93744d5b9c58c
xihajun <www.junfan.com@gmail.com>
2019-02-02 19:43:27 +0000

    autosave

--- Git/autosave.sh
+++ Git/autosave.sh
@@ -1,11 +1,13 @@
 LOGFILE=./upload.log
 git add --all
 git commit -m "autosave"
+#try catch function 
+#write in this way {code1&&}||{code2}
 {
    git push&&
    echo "$(date) : file uploaded" >> $LOGFILE
 }||{
    git pull --rebase
    git push
-   echo "`date +%H:%M:%S` : file reloaded and uploaded" >> $LOGFILE
+   echo "$(date) : file reloaded and uploaded" >> $LOGFILE
 }
