a3fadf8b25afd19804e8887e03a9c969b73015ec
xihajun <www.junfan.com@gmail.com>
2019-02-02 19:06:12 +0000

    autosave

--- Git/autosave.sh
+++ Git/autosave.sh
@@ -1,8 +1,11 @@
+LOGFILE="/User/xihajun/Documents/Data Science/data-science-toolbox-kate-syd-jun/Git/upload.log"
 git add --all
-git commit -m "autosave-j"
+git commit -m "autosave"
 {
    git push
+   echo "`date +%H:%M:%S : file uploaded" >> $LOGFILE
 }||{
    git pull --rebase
    git push
+   echo "`date +%H:%M:%S : file reloaded and uploaded" >> $LOGFILE
 }
