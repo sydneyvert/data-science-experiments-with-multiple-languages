bb93194aa8e928b4e7c0e5f69ebabfe50128d9ae
xihajun <www.junfan.com@gmail.com>
2019-02-03 12:25:05 +0000

    autosave

--- Git/autosave.sh
+++ Git/autosave.sh
@@ -1,9 +1,13 @@
 LOGFILE=./upload.log
 git checkout master
-git add --all
+git add .
 git commit -m "autosave"
 #try catch function 
 #write in this way {code1&&}||{code2}
+read -p "请选择相应的发行版本系统（输入数字序号）：" number
+if [ "$number" == "" ]; then
+  echo "未选择任何发行版本，脚本退出"
+  exit 1
 {
    git push&&
    echo "$(date) : file uploaded" >> $LOGFILE
