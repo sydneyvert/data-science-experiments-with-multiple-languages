95514f406c2c7b120d13f2f93835dfca89b19b6e
sydneyvert <40839435+sydneyvert@users.noreply.github.com>
2019-02-03 13:36:56 +0000

    Add files via upload

--- Documents/Syd_try.py
+++ Documents/Syd_try.py
@@ -22,4 +22,36 @@ data.columns = ['original', 'split']
 for index, row in data.iterrows():
     row['split'] = list(row['original'])
 
+import re
 
+s2 ='letter123num45***'
+
+s2_split = re.split('(\d+)',s2)
+
+data["numletsplit"] = ""
+
+for index, row in data.iterrows():
+    row['numletsplit'] = re.split('(\d+)', row['original'])
+    
+import wordsegment as ws
+
+from wordsegment import load, segment
+
+seg = 'testmysplitsegmentpleaseandthankyou'
+
+load()
+segtry = segment(seg)
+
+#works for individual words in lists:
+segtry = segment((data.iloc[1]['numletsplit'])[0])
+
+new_list = list()
+for i, val in enumerate(data.iloc[53]['numletsplit']):
+    new_list.append(segment((data.iloc[53]['numletsplit'])[i]))
+
+data["extrawordsplit"] = ""
+
+for index, row in data.iterrows():
+    row['extrawordsplit'] = list()
+    for ind, val in enumerate(row['numletsplit']):
+        row['extrawordsplit'].append(segment((row['numletsplit'])[ind]))
